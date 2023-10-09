d57aaf9649ec3cfc7309feb7d26b64c54a11c39d
Will ARTHUR <will.arthur@hotmail.com>
2023-10-09 13:06:31 +0100

    Initial commit

---
+++ getDateTime.js
@@ -0,0 +1,15 @@
+const currentDate = document.getElementById("current_date");
+const currentTime = document.getElementById("current_time");
+
+let date = new Date();
+let getCurrentDate = date.toLocaleDateString("en-UK", {
+  day: "numeric",
+  month: "long",
+  year: "numeric",
+});
+let getCurrentTime =
+  " " +
+  date.toLocaleTimeString("en-UK", { timeStyle: "short", hour12: "true" });
+
+currentDate.innerText = getCurrentDate;
+currentTime.innerText = getCurrentTime;
