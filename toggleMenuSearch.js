87f3b953fefb40d67aaeca7a27843a8f17364bcc
Wil Arthur <will.arthur@hotmail.com>
2023-10-10 14:00:49 +0100

    Added mobile menu and button toggle script

--- toggleMenuSearch.js
+++ toggleMenuSearch.js
@@ -2,6 +2,7 @@ const desktopSearchBtn = document.getElementById("open-desktop-search");
 const mobileSearchBtn = document.getElementById("open-mobile-search");
 const mobileMenuBtn = document.getElementById("open-mobile-menu");
 const searchBar = document.getElementById("search-bar");
+const mobileMenu = document.getElementById("mobile-menu");
 
 function toggleSearchBar() {
   if (searchBar.style.display === "none") {
@@ -11,7 +12,13 @@ function toggleSearchBar() {
   }
 }
 
-function toggleMobileMenu() {}
+function toggleMobileMenu() {
+  if (mobileMenu.style.display === "none") {
+    mobileMenu.style.display = "flex";
+  } else {
+    mobileMenu.style.display = "none";
+  }
+}
 
 desktopSearchBtn.addEventListener("click", toggleSearchBar);
 mobileSearchBtn.addEventListener("click", toggleSearchBar);
