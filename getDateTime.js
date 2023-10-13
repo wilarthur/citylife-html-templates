fd4065691bed15502a268db7195e5fe5684e54fd
Wil Arthur <will.arthur@hotmail.com>
2023-10-13 14:50:46 +0100

    Updated with regex to remove leading 0

--- getDateTime.js
+++ getDateTime.js
@@ -5,10 +5,13 @@ let date = new Date();
 let getCurrentDate = date.toLocaleDateString("en-UK", {
   day: "numeric",
   month: "long",
-  year: "numeric",
 });
 let getCurrentTime =
-  "" + date.toLocaleTimeString("en-UK", { timeStyle: "short", hour12: "true" });
+  "" +
+  date.toLocaleTimeString("en-UK", {
+    timeStyle: "short",
+    hour12: "true",
+  });
 
 currentDate.innerText = getCurrentDate;
-currentTime.innerText = getCurrentTime;
+currentTime.innerText = getCurrentTime.replace(/^0+/, "");
