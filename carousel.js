76dc2e5f3412b8bd779cc024cd2c0f52b48b23bd
Wil Arthur <will.arthur@hotmail.com>
2023-11-08 16:00:08 +0000

    Added slick carousel and put scripts in own files

--- carousel.js
+++ carousel.js
@@ -1,30 +1,10 @@
-//Set slideIndex to 1 by default
-let carIndex = 1;
-showCar(carIndex);
-
-//Set modal image to the same image as what is clicked
-function currentCar(n) {
-  showCar((carIndex = n));
-}
-
-function showCar(n) {
-  const carImgs = document.getElementsByClassName("carousel-image");
-
-  //If number is greater than number of images, cycle back to the first
-  if (n > carImgs.length) {
-    carIndex = 1;
-  }
-
-  //If number is less than the first image, cycle to the last image in the index
-  if (n < 1) {
-    carIndex = carImgs.length;
-  }
-
-  //Set all images to display: none
-  for (let i = 0; i < carImgs.length; i++) {
-    carImgs[i].style.display = "none";
-  }
-
-  //Set correct image to display in main container
-  carImgs[carIndex - 1].style.display = "block";
-}
+$(document).ready(function () {
+  $(".carousel").slick({
+    accessibility: true,
+    arrows: true,
+    prevArrow:
+      '<button class="slick-prev"><img src="/images/icons/chevron-left.svg">',
+    nextArrow:
+      '<button class="slick-next"><img src="/images/icons/chevron-right.svg">',
+  });
+});
--- lightboxGallery.js
+++ lightboxGallery.js
@@ -1,49 +1,18 @@
-let lightbox = document.getElementById("lightboxOverlay");
-let closeLightboxBtn = document.getElementById("closeLightbox");
-
-function openLightbox() {
-  lightbox.style.display = "flex";
-  closeLightboxBtn.focus();
-  document.documentElement.style.overflow = "hidden";
-}
-
-function closeLightbox() {
-  lightbox.style.display = "none";
-  document.documentElement.style.overflow = "auto";
-}
-
-//Set slideIndex to 1 by default
-let slideIndex = 1;
-showSlides(slideIndex);
-
-//Calculate index when next/previous clicked in modal
-function changeSlide(n) {
-  showSlides((slideIndex += n));
-}
-
-//Set modal image to the same image as what is clicked
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  const slides = document.getElementsByClassName("slide-image");
-
-  //If number is greater than number of images, cycle back to the first
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-
-  //If number is less than the first image, cycle to the last image in the index
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  //Set all images to display: none
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  //Set correct image to display in main container
-  slides[slideIndex - 1].style.display = "block";
-}
+$(document).ready(function () {
+  $(".gallery").each(function () {
+    // the containers for all your galleries
+    $(this).magnificPopup({
+      delegate: "a", // the selector for gallery item
+      type: "image",
+      gallery: {
+        enabled: true,
+        navigateByImgClick: true,
+        arrowMarkup:
+          '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
+        tPrev: "Previous (Left arrow key)", // title for left button
+        tNext: "Next (Right arrow key)", // title for right button
+        tCounter: '<span class="mfp-counter">%curr% of %total%</span>', // markup of counter
+      },
+    });
+  });
+});
