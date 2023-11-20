4a4b65e50fb586762940382284142a7cf0053758
Wil Arthur <will.arthur@hotmail.com>
2023-11-20 15:09:14 +0000

    Added captions to galleries and carousels

--- lightboxGallery.js
+++ lightboxGallery.js
@@ -11,7 +11,8 @@ $(document).ready(function () {
           '<button title="%title%" type="button" class="mfp-arrow mfp-arrow-%dir%"></button>', // markup of an arrow button
         tPrev: "Previous (Left arrow key)", // title for left button
         tNext: "Next (Right arrow key)", // title for right button
-        tCounter: '<span class="mfp-counter">%curr% of %total%</span>', // markup of counter
+        tCounter: '<span class="mfp-counter">%curr% of %total%</span>',
+        titleSrc: "title", // markup of counter
       },
     });
   });
